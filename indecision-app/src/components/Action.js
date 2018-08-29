718c4afdf5b6afe8b053fe77f02958e5253d559b
pavi <pavi>
2018-08-28 23:59:15 -0500

    Indecision app

---
+++ indecision-app/src/components/Action.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Action = (props) => (
+    <div>
+        <button onClick={props.handlePick} disabled={!props.hasOptions}>
+            What should we do?
+        </button>
+    </div>
+);
+
+export default Action;
